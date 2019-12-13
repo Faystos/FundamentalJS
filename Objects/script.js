b7d436958565f7ec4962494cd3d779bbc6517b29
Vladimir <fayst_@bk.ru>
2019-12-13 21:22:39 +0300

    Выполнил задания по уроку 'Объекты'.

---
+++ Objects/script.js
@@ -0,0 +1,13 @@
+let obj = {
+  product: 'iphone'
+};
+
+obj.price = 1000;
+obj.currency = 'dollar';
+obj.details = {};
+obj.details.model = '5s';
+obj.details.color = 'black';
+
+
+
+console.log(obj);
