60a8ee98f839f76047021095d9959901b69c75f1
Faystos <fayst_@bk.ru>
2019-12-16 20:47:51 +0300

    Доделал задания по уроку 'условные операторы'.

--- if_statement/script.js
+++ if_statement/script.js
@@ -10,6 +10,18 @@
 
 4. Дан объект let item = { name: 'Intel core i7', price: '100$', discount: '15%' }.
 Написать условие если у item есть поле discount и там есть значение то в объекте item создать поле priceWithDiscount и записать туда цену с учетом скидки и вывести ее в консоль, обратите внимание  что поля discount и price это строки и вам из них нужно получить числа чтобы выполнить расчет. иначе если поля discount нет то вывести просто поле price в консоль.
+
+5. Дан следующий код:
+
+let product = {
+    name: “Яблоко”,
+    price: “10$”
+};
+
+let min = 10; // минимальная цена
+let max = 20; // максимальная цена
+
+Написать условие если цена товара больше или равна минимальной цене и меньше или равна максимальной цене то вывести в консоль название этого товара, иначе вывести в консоль что товаров не найдено.
 */
 
 let a = 'hidden';
@@ -19,8 +31,7 @@ if (a === 'hidden') {
   a = 'hidden';  
 }
 
-let b = -1;
-
+let b = 0;
 if (b === 0){
   b = 1;
 } else if (b < 0) {
@@ -36,9 +47,10 @@ let car = {
   needRepair: false
 };
 
-if (car.age > 5) {
-  // console.log('Need Repair');
+if (car.age > 5) {  
   car.needRepair = true;
+} else {
+  car.needRepair = false;
 }
 
 let item = {
@@ -48,17 +60,22 @@ let item = {
 };
 
 if (item.discount) {  
-  item.priceWithDiscount = Math.round((parseInt(item.price) / 100) * parseInt(item.discount));
-  console.log(item);
+  item.priceWithDiscount = (parseInt(item.price) - (Math.round((parseInt(item.price) / 100) * parseInt(item.discount))))+ '$';
+  console.log(item.priceWithDiscount);
+} else { 
+  console.log(item.price);
 }
 
+let product = {
+  name: 'Яблоко',
+  price: '10$'
+};
 
+let min = 10; 
+let max = 20; 
 
-
-
-
-
-
-
-
-
+if (parseInt(product.price) >= min && parseInt(product.price) <= max) {
+  console.log(product.name);
+} else {
+  console.log('товаров не найдено');
+}
