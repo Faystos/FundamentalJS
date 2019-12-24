d87cd0f08988bfb17935a87b706b5df8fb0b9bed
Faystos <fayst_@bk.ru>
2019-12-24 16:06:37 +0300

    Выполнил задания по уроку 'Деструктуризация'.

---
+++ destructurization/script.js
@@ -0,0 +1,55 @@
+/*
+1. Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+
+func(‘a’, ‘b’, ‘c’, ‘d’) → 
+{
+  first: ‘a’,
+  other: [‘b’, ‘c’, ‘d’]
+}
+
+2. Организовать функцию getInfo, которая принимает объект вида
+{ name: ...,  info: { employees: [...], partners: [ … ]  } }
+и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+const organisation = {  
+  name: 'Google',   
+  info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+};
+getInfo(organisation); → 
+Name: Google 
+Partners: Microsoft Facebook
+*/
+
+// 1
+function foo (a, b, c, d) {
+  const arr = [...arguments];
+  [a] = arr;
+  [, ...other] = arr;    
+  return {
+    first: a,
+    other: other
+  };
+}
+
+let test = foo(1, 2, 3, 4);
+// --------------------------------------------------------------------------------------------------
+
+// 2
+const organisation = {  
+  name: 'Google', 
+  info: {
+    employees: ['Vlad', 'Olga'],
+    partners: ['Microsoft', 'Facebook', 'Xing']
+  } 
+};
+
+function getInfo ({
+  name = 'Unknown',
+  info: { partners: [a, b]}
+} = {}) {
+  console.log(`name: ${name}`);
+  console.log(`Partners: ${a}, ${b}`);
+}
+
+getInfo(organisation);
+// -------------------------------------------------------------------------------------------------
