3dbae153913a1f218de77f12c2927606a3d4a758
Faystos <fayst_@bk.ru>
2020-01-11 02:07:00 +0300

    Внес небольшие правки по стилю кода

--- eventDomJs/task2/script.js
+++ eventDomJs/task2/script.js
@@ -1,19 +1,13 @@
 let menu = document.querySelector('.menu');
 let check = '';
 
-
-
 menu.addEventListener('click', evt => { 
-
-  if(!evt.target.closest('.dropdown-item')) {
-    return;
-  }
+  if(!evt.target.closest('.dropdown-item')) return;
    menu.querySelectorAll('.dropdown-menu').forEach(el => el.classList.add('d-none'));
-
    if(check !== evt.target.closest('.dropdown-item')){
     check = evt.target.closest('.dropdown-item');
     check.querySelector('.dropdown-menu').classList.remove('d-none');
   } else {
     check = '';
   }
-})
+});
