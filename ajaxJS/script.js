8d6dfe96d2e8735ca018cc45ed7b148e61290185
Vladimir <fayst_@bk.ru>
2020-01-16 21:22:21 +0300

    Начал пилить второе задание с post запросом.

--- ajaxJS/script.js
+++ ajaxJS/script.js
@@ -37,7 +37,7 @@
     response.forEach(user => {
       const listLi = document.createElement('li');
       listLi.classList.add('list_user');
-      listLi.textContent = `${user.id}.`;      
+      listLi.textContent = `${user.id}.` || '';      
       const userNameP = document.createElement('p');
       userNameP.classList.add('parag_user_name');
       userNameP.textContent = `${user.name}`;
@@ -124,5 +124,74 @@
   
   btnGet.addEventListener('click', () => {
     getRequest('GET', urlGet,onSuccess, onError);
+  });
+
+
+
+  // обработка формы.
+
+  let userEmail = document.querySelector('#exampleInputEmail1');
+  let userName = document.querySelector('#formGroupExampleInput1');
+  let userLogin = document.querySelector('#formGroupExampleInput2');
+  let btnSubm = document.querySelector('.btn_submit');
+
+  function createBodyForm () {
+    let userEmailValue = userEmail.value;
+    let userNameValue = userName.value;
+    let userLoginValue = userLogin.value;
+    const bodyForm = {
+      email: userEmailValue,
+      name: userNameValue,
+      username: userLoginValue,
+      phone: 'Нет информации',
+      address: {
+        city: 'Нет информации',
+        street: 'Нет информации'
+      }
+    };
+
+    return bodyForm;
+  }
+
+  function postRequest (method, url, onSuccess, onError, body) {
+    try {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+    // "Content-type": "application/json; charset=UTF-8"
+      xhr.addEventListener('load', () => {        
+        if (Math.floor(xhr.status / 100) !== 2) onError(`Возникла ошибка: ${xhr.status}`, xhr);
+        const response = JSON.parse(xhr.responseText);
+        onSuccess(response);      
+      });
+
+      xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+      xhr.addEventListener('error', () => {
+        onError(`Возникла ошибка: ${xhr.status}`, xhr);
+      });    
+    
+      xhr.send(JSON.stringify(body));
+    } catch (error) {
+      onError(error);
+    }    
+  }
+
+  function postOnSuccess (response) {    
+    const postPesponse = [];
+    postPesponse.push(response);
+    blockUserName.appendChild(inputNameUser(postPesponse));
+    inputInfoUsers(postPesponse);
+    
+    
+  }
+
+  function postOnError (error){
+    console.log(error);
+  }
+
+  btnSubm.addEventListener('click', evt => {
+    evt.preventDefault();
+    let bodyForm = createBodyForm();    
+    postRequest('POST', 'https://jsonplaceholder.typicode.com/posts', postOnSuccess, postOnError, bodyForm);
   });
 })();
